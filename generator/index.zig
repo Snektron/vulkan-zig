74d6846d68bbe0ff795d0332d8d4d4d5662d0201
Robin Voetter <robin@voetter.nl>
2020-08-11 17:40:02 +0200

    Use underscores instead of dash in filenames

--- generator/index.zig
+++ generator/index.zig
@@ -1,6 +1,6 @@
 pub const generateVk = @import("vulkan/generator.zig").generate;
-pub const VkGenerateStep = @import("vulkan/build-integration.zig").GenerateStep;
-pub const ShaderCompileStep = @import("build-integration.zig").ShaderCompileStep;
+pub const VkGenerateStep = @import("vulkan/build_integration.zig").GenerateStep;
+pub const ShaderCompileStep = @import("build_integration.zig").ShaderCompileStep;
 
 test "main" {
     _ = @import("xml.zig");
