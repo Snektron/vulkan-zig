9e407216552816c965a0a2335aec58503ba89a78
Robin Voetter <robin@voetter.nl>
2021-09-02 16:16:35 +0200

    Update to new usingnamespace semantics

--- examples/c.zig
+++ examples/c.zig
@@ -4,10 +4,11 @@ pub usingnamespace @cImport({
 });
 
 const vk = @import("vulkan");
+const c = @This();
 
 // usually the GLFW vulkan functions are exported if Vulkan is included,
 // but since  thats not the case here, they are manually imported.
 
 pub extern fn glfwGetInstanceProcAddress(instance: vk.Instance, procname: [*:0]const u8) vk.PfnVoidFunction;
 pub extern fn glfwGetPhysicalDevicePresentationSupport(instance: vk.Instance, pdev: vk.PhysicalDevice, queuefamily: u32) c_int;
-pub extern fn glfwCreateWindowSurface(instance: vk.Instance, window: *GLFWwindow, allocation_callbacks: ?*const vk.AllocationCallbacks, surface: *vk.SurfaceKHR) vk.Result;
+pub extern fn glfwCreateWindowSurface(instance: vk.Instance, window: *c.GLFWwindow, allocation_callbacks: ?*const vk.AllocationCallbacks, surface: *vk.SurfaceKHR) vk.Result;
