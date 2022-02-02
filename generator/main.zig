0f496af7e92959d7e5002812dde42ee7accd04f8
Robin Voetter <robin@voetter.nl>
2022-02-02 17:09:11 +0100

    Adapt to new ArgsIterator api

--- generator/main.zig
+++ generator/main.zig
@@ -10,13 +10,13 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var args = std.process.args();
-    const prog_name = (try args.next(allocator)) orelse return error.ExecutableNameMissing;
+    var args = try std.process.argsWithAllocator(allocator);
+    const prog_name = args.next() orelse return error.ExecutableNameMissing;
 
     var maybe_xml_path: ?[]const u8 = null;
     var maybe_out_path: ?[]const u8 = null;
 
-    while (try args.next(allocator)) |arg| {
+    while (args.next()) |arg| {
         if (std.mem.eql(u8, arg, "--help") or std.mem.eql(u8, arg, "-h")) {
             @setEvalBranchQuota(2000);
             try stderr.writer().print(
