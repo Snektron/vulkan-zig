500d7cc3e8cd3606cd4574ef8f0ff5c17f9b4c2a
dan <i@dan.games>
2024-04-24 11:10:42 +0800

    fix: change ComptimeStringMap to StaticStringMap
    reflect changes made in https://github.com/ziglang/zig/commit/8af59d1f98266bd70b3afb44d196bbd151cedf22

--- generator/vulkan/render.zig
+++ generator/vulkan/render.zig
@@ -96,7 +96,7 @@ const preamble =
     \\
 ;
 
-const builtin_types = std.ComptimeStringMap([]const u8, .{
+const builtin_types = std.StaticStringMap([]const u8).initComptime(.{
     .{ "void", @typeName(void) },
     .{ "char", @typeName(u8) },
     .{ "float", @typeName(f32) },
@@ -113,7 +113,7 @@ const builtin_types = std.ComptimeStringMap([]const u8, .{
     .{ "int", @typeName(c_int) },
 });
 
-const foreign_types = std.ComptimeStringMap([]const u8, .{
+const foreign_types = std.StaticStringMap([]const u8).initComptime(.{
     .{ "Display", "opaque {}" },
     .{ "VisualID", @typeName(c_uint) },
     .{ "Window", @typeName(c_ulong) },
@@ -425,13 +425,13 @@ fn Renderer(comptime WriterType: type) type {
         }
 
         fn classifyCommandDispatch(name: []const u8, command: reg.Command) CommandDispatchType {
-            const device_handles = std.ComptimeStringMap(void, .{
+            const device_handles = std.StaticStringMap(void).initComptime(.{
                 .{ "VkDevice", {} },
                 .{ "VkCommandBuffer", {} },
                 .{ "VkQueue", {} },
             });
 
-            const override_functions = std.ComptimeStringMap(CommandDispatchType, .{
+            const override_functions = std.StaticStringMap(CommandDispatchType).initComptime(.{
                 .{ "vkGetInstanceProcAddr", .base },
                 .{ "vkCreateInstance", .base },
                 .{ "vkEnumerateInstanceLayerProperties", .base },
